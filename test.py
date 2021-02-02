f54b5559377ac8e89b5f2f6ee97ae2fc8fbf124c
CallMeChou <63540671+CallMeChou@users.noreply.github.com>
2021-02-02 12:09:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import time
+import requests
 ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-print(ti)
-with open("1.txt","a+") as f:
-    f.write(ti)
-
+r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+ti)
   
   
