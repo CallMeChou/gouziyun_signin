5cc3bab786c02cdcd98e12e4e6b3b14579f87cca
CallMeChou <63540671+CallMeChou@users.noreply.github.com>
2021-02-03 10:43:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,15 @@ import os
 import argparse
 # ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-username = os.environ["EMAIL"]        
-password = os.environ["PASSWORD"]
+username1 = os.environ["EMAIL1"]        
+password1 = os.environ["PASSWORD1"]
+username2 = os.environ["EMAIL2"]        
+password2 = os.environ["PASSWORD2"]
 
-parser = argparse.ArgumentParser(description='auto signin script.')
-parser.add_argument('stuid', help='your student number', type=str)
-parser.add_argument('password', help='your CAS password', type=str)
-args = parser.parse_args()
-r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+username[0])
-r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+username)
+# parser = argparse.ArgumentParser(description='auto signin script.')
+# parser.add_argument('stuid', help='your student number', type=str)
+# parser.add_argument('password', help='your CAS password', type=str)
+# args = parser.parse_args()
+r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+username1)
+r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+username2)
 
