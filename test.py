dd5f8c1f52f061ce8dff99ac4413976b5f2ff2f3
CallMeChou <63540671+CallMeChou@users.noreply.github.com>
2021-02-03 09:25:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import time
 import requests
+import os
 ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+ti)
-with open(r"./1.txt","a+") as f:
-  f.write(ti)
-  
+with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
+        f.write(ti)
   
