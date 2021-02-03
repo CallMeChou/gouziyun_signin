402ab082ac50e25d91869a41fc8d8c9e0efdc339
CallMeChou <63540671+CallMeChou@users.noreply.github.com>
2021-02-03 10:14:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import time
 import requests
 import os
-ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+ti)
-with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
-        f.write(ti)
-  
+import argparse
+# ti = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+username = os.environ["EMAIL"]        
+password = os.environ["PASSWORD"]
+
+parser = argparse.ArgumentParser(description='auto signin script.')
+parser.add_argument('stuid', help='your student number', type=str)
+parser.add_argument('password', help='your CAS password', type=str)
+args = parser.parse_args()
+r=requests.post("https://sc.ftqq.com/SCU157350Te9a01b046907e9e3ef2e03af71dbe5fa6018c6d67e1a4.send?text="+username)
+
+
